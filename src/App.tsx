78ced44a582c2a76ec0c6eb90f02dcc12e5f67a4
IvanRomero03 <i.wells.ar@gmail.com>
2022-09-25 03:38:10 -0500

    HEATMAP

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,232 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import useRef, { useState } from "react";
+import styles from "./styles/Home.module.css";
+import { Tabs, Text, Box, Checkbox, Divider } from "@mantine/core";
+import GoogleHeatMap from "./components/GoogleHeatMap";
+import AzureLayer from "./components/AzureLayer";
 
 function App() {
+  const [checked, setChecked] = useState<string | string[]>("escob");
+  console.log(checked);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Tabs color="dark" defaultValue="Home">
+        <Tabs.List>
+          <Tabs.Tab value="Home">
+            <Text color={"dark"}>Home</Text>
+          </Tabs.Tab>
+          <Tabs.Tab value="Presion">
+            <Text color={"dark"}>Presion</Text>
+          </Tabs.Tab>
+          <Tabs.Tab value="Calidad">
+            <Text color={"dark"}>Calidad</Text>
+          </Tabs.Tab>
+          <Tabs.Tab value="Precipitacion">
+            <Text color={"dark"}>Precipitacion</Text>
+          </Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel value="Home" pt="xs">
+          <div className={styles.container}>
+            <head>
+              <title>ORG NAME</title>
+              <meta
+                name="description"
+                content="Water managmanet and identification"
+              />
+              <link rel="icon" href="/favicon.ico" />
+            </head>
+
+            <main className={styles.main}>
+              <h1 className={styles.title}>NOMBRE ORG</h1>
+
+              <p className={styles.description}>
+                Borregos Operating System (BOS)
+              </p>
+
+              <div className={styles.grid}>
+                <a href="https://nextjs.org/docs" className={styles.card}>
+                  <h2>Modelo de IA &rarr;</h2>
+                  <p>
+                    Utilizamos un modelo de inteligancia artificial el cual es
+                    capaz de predecir la calidad del agua a partir de diferentes
+                    variables
+                  </p>
+                </a>
+
+                <a href="https://nextjs.org/learn" className={styles.card}>
+                  <h2>Sensores &rarr;</h2>
+                  <p>
+                    Descubran los sensores utilizados, los cuales miden las
+                    particulas por millon y el flujo del agua, para que este
+                    proyecto fuese posible
+                  </p>
+                </a>
+
+                <a
+                  href="https://github.com/vercel/next.js/tree/canary/examples"
+                  className={styles.card}
+                >
+                  <h2>Examples &rarr;</h2>
+                  <p>
+                    Discover and deploy boilerplate example Next.js projects.
+                  </p>
+                </a>
+
+                <a
+                  href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                  className={styles.card}
+                >
+                  <h2>Deploy &rarr;</h2>
+                  <p>
+                    Instantly deploy your Next.js site to a public URL with
+                    Vercel.
+                  </p>
+                </a>
+              </div>
+            </main>
+
+            <footer className={styles.footer}>
+              <a
+                href="https://hackmty.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Powered by HackMTY
+              </a>
+            </footer>
+          </div>
+        </Tabs.Panel>
+
+        <Tabs.Panel value="Presion" pt="xs">
+          <Checkbox.Group
+            defaultValue={["allpoints"]}
+            label="Puntos de medición"
+            description="Seleccione los puntos de medición que desea visualizar"
+            onChange={(value) => setChecked(value)}
+          >
+            <Checkbox value="allpoints" label="Todos los puntos" />
+            <Checkbox value="escob" label="Escobedo" />
+            <Checkbox value="sanped" label="San Pedro" />
+            <Checkbox value="sannico" label="San Nicolas" />
+            <Checkbox value="monterrey" label="Monterrey" />
+            <Checkbox value="apoda" label="Apodaca" />
+            <Checkbox value="cumb" label="Cumbres" />
+            <Checkbox value="sant" label="Santiago" />
+            <Checkbox value="guad" label="Guadalupe" />
+          </Checkbox.Group>
+
+          <Divider size={20} />
+
+          <Box
+            sx={(theme) => ({
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[6]
+                  : theme.colors.gray[0],
+              textAlign: "center",
+              padding: theme.spacing.xl,
+              borderRadius: theme.radius.md,
+              cursor: "pointer",
+
+              "&:hover": {
+                backgroundColor:
+                  theme.colorScheme === "dark"
+                    ? theme.colors.dark[5]
+                    : theme.colors.gray[1],
+              },
+            })}
+          >
+            AGREGRAR HEATMAP
+            <AzureLayer />
+          </Box>
+        </Tabs.Panel>
+        <Tabs.Panel value="Calidad" pt="xs">
+          <Checkbox.Group
+            defaultValue={["allpoints"]}
+            label="Puntos de medición"
+            description="Seleccione los puntos de medición que desea visualizar"
+            onChange={(value) => setChecked(value)}
+          >
+            <Checkbox value="allpoints" label="Todos los puntos" />
+            <Checkbox value="escob" label="Escobedo" />
+            <Checkbox value="sanped" label="San Pedro" />
+            <Checkbox value="sannico" label="San Nicolas" />
+            <Checkbox value="monterrey" label="Monterrey" />
+            <Checkbox value="apoda" label="Apodaca" />
+            <Checkbox value="cumb" label="Cumbres" />
+            <Checkbox value="sant" label="Santiago" />
+            <Checkbox value="guad" label="Guadalupe" />
+          </Checkbox.Group>
+
+          <Divider size={20} />
+
+          <Box
+            sx={(theme) => ({
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[6]
+                  : theme.colors.gray[0],
+              textAlign: "center",
+              padding: theme.spacing.xl,
+              borderRadius: theme.radius.md,
+              cursor: "pointer",
+
+              "&:hover": {
+                backgroundColor:
+                  theme.colorScheme === "dark"
+                    ? theme.colors.dark[5]
+                    : theme.colors.gray[1],
+              },
+            })}
+          >
+            AGREGRAR HEATMAP
+          </Box>
+        </Tabs.Panel>
+
+        <Tabs.Panel value="Precipitacion" pt="xs">
+          <Checkbox.Group
+            defaultValue={["allpoints"]}
+            label="Puntos de medición"
+            description="Seleccione los puntos de medición que desea visualizar"
+            onChange={(value) => setChecked(value)}
+          >
+            <Checkbox value="allpoints" label="Todos los puntos" />
+            <Checkbox value="escob" label="Escobedo" />
+            <Checkbox value="sanped" label="San Pedro" />
+            <Checkbox value="sannico" label="San Nicolas" />
+            <Checkbox value="monterrey" label="Monterrey" />
+            <Checkbox value="apoda" label="Apodaca" />
+            <Checkbox value="cumb" label="Cumbres" />
+            <Checkbox value="sant" label="Santiago" />
+            <Checkbox value="guad" label="Guadalupe" />
+          </Checkbox.Group>
+
+          <Divider size={20} />
+
+          <Box
+            sx={(theme) => ({
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[6]
+                  : theme.colors.gray[0],
+              textAlign: "center",
+              padding: theme.spacing.xl,
+              borderRadius: theme.radius.md,
+              cursor: "pointer",
+
+              "&:hover": {
+                backgroundColor:
+                  theme.colorScheme === "dark"
+                    ? theme.colors.dark[5]
+                    : theme.colors.gray[1],
+              },
+            })}
+          ></Box>
+        </Tabs.Panel>
+      </Tabs>
+    </>
   );
 }
 
---
+++ src/components/AzureLayer.tsx
@@ -0,0 +1,108 @@
+import React, { useMemo } from "react";
+import {
+  AzureMapsProvider,
+  AzureMap,
+  AzureMapDataSourceProvider,
+  AzureMapLayerProvider,
+  AzureMapFeature,
+  AzureMapHtmlMarker,
+  IAzureMapOptions,
+  IAzureMapHtmlMarkerEvent,
+} from "react-azure-maps";
+import { AuthenticationType, data } from "azure-maps-control";
+// import { key } from "../key";
+import { Text } from "@mantine/core";
+
+export const wrapperStyles = {
+  map: {
+    height: "350px",
+  },
+  wrapper: {
+    padding: "15px",
+    marginTop: "15px",
+  },
+  buttonContainer: {
+    display: "grid",
+    gridAutoFlow: "column",
+    gridGap: "10px",
+    gridAutoColumns: "max-content",
+    padding: "10px 0",
+  },
+  buttons: {
+    padding: "15px",
+    flex: 1,
+  },
+  popupStyles: {
+    padding: "20px",
+    color: "black",
+  },
+};
+
+function getCoordinates(e: any) {
+  console.log("Clicked on:", e.position);
+}
+
+const onClick = () => {
+  console.log("ASD");
+};
+
+const azureHtmlMapMarkerOptions = {
+  position: [-110, 45],
+};
+
+const eventToMarker: Array<IAzureMapHtmlMarkerEvent> = [
+  { eventName: "click", callback: onClick },
+];
+const option: IAzureMapOptions = {
+  authOptions: {
+    authType: "subscriptionKey" as any,
+    subscriptionKey: "WwfO6BKjULP5qQn1LbqzBb84wB9cXN7XnOMiK5MKlkc",
+  },
+  center: [-100.01, 45.01],
+  zoom: 2,
+  view: "Auto",
+};
+const AzureLayer: React.FC = () => {
+  const point = new data.Position(-80.01, 35.01, 1);
+  const point1 = new data.Position(-100.01, 45.01);
+  const point2 = new data.Position(-100.01, 45.01);
+
+  return (
+    <>
+      <div style={wrapperStyles.map}>
+        <Text>
+          This sample shows how to add layers and global map events. Open dev
+          tools console and click on map.
+        </Text>
+        <AzureMapsProvider>
+          <div style={styles.map}>
+            <AzureMap options={option}>
+              <AzureMapDataSourceProvider id={"LayerExample2 DataSource"}>
+                <AzureMapLayerProvider
+                  id={"LayerExample2 HeatMap"}
+                  options={{}}
+                  type={"HeatLayer"}
+                />
+                <AzureMapFeature
+                  id={"LayerExample2 MapFeature2"}
+                  key={"dddd"}
+                  type="MultiPoint"
+                  coordinates={[point, point1, point2]}
+                />
+              </AzureMapDataSourceProvider>
+            </AzureMap>
+          </div>
+        </AzureMapsProvider>
+      </div>
+    </>
+  );
+};
+
+const styles = {
+  map: {
+    height: 300,
+    marginBottom: 50,
+  },
+};
+
+export default AzureLayer;
